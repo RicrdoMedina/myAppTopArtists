19e6c38bcbd2fd8b2f718770c265ab603ab266ba
Ricardo Medina <rcrdmedina6@gmail.com>
2017-12-21 11:00:28 -0430

    Configurando la api de last.fm en el proyecto

---
+++ src/api/config.js
@@ -0,0 +1,16 @@
+
+// API account created
+// Here are the details of your new API account.
+
+// Application name	myprojectmusic
+// API key	530008d1e1c32fb9159f1b10901f5032
+// Shared secret	4f7e95c3a219d7be6f61ebe499026f4d
+// Registered to	rcrdmedina6
+
+export default {
+  appName: 'myprojectmusic',
+  apiKey: '530008d1e1c32fb9159f1b10901f5032',
+  secret: '4f7e95c3a219d7be6f61ebe499026f4d',
+  RegisteredTo:	'rcrdmedina6'
+}
+
