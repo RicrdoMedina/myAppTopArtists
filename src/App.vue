cd53bae39f59300bfd0ebef58111c9d7f6cd5ae8
Ricardo Medina <rcrdmedina6@gmail.com>
2017-12-21 16:52:29 -0430

    Se agrego select por region para actualizar los artistas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   #app
     img(src='./assets/logo.png')
-    h2 Project Music
+    h1 Project Music
+    select(v-model="selectedCountry")
+      option(v-for="country in counties" v-bind:value="country.value") {{ country.name }}
     ul
       artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
 </template>
@@ -14,18 +16,34 @@ export default {
   name: 'app',
   data () {
     return {
-      artists: []
+      artists: [],
+      counties: [
+        { name : 'Argentina', value : 'argentina' },
+        { name : 'Colombia', value : 'colombia' },
+        { name : 'España', value : 'spain' }
+      ],
+      selectedCountry: 'argentina'
     }
   },
   components:{
     Artist
   },
-  mounted: function () {
-    const self = this
-    getArtists()
-      .then( function (artists) {
-        self.artists = artists
-      })
+  methods: {
+    refreshArtist() {
+      const self = this
+      getArtists(this.selectedCountry)
+        .then( function (artists) {
+          self.artists = artists
+        })
+    }
+  },
+  mounted () {
+   this.refreshArtist()
+  },
+  watch: {
+    selectedCountry () {
+      this.refreshArtist()
+    }
   }
 }
 </script>
